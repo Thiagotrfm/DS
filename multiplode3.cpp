fbe6d46334b25138842ec0bc6efebb891bc631f2
Thiagotrfm <t_moreira@outlook.com>
2017-09-20 10:57:14 -0300

    Mostrando o resultado

--- multiplode3.cpp
+++ multiplode3.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 long somarDigitos(string numero){
+	int cont = 0;
 	for(int i=0; i<(int)numero.size(); ++i){
 		int num = numero[i]-48;
 		cont+=num;
@@ -17,5 +18,11 @@ int main(){
 	cin >> numero;
 	
 	somaDosDigitos = somarDigitos(numero);
+	
+	if(somaDosDigitos%3==0)
+		cout << "Multiplo de 3" << endl;
+	else
+		cout << "Não múltiplo de 3" << endl;
+
 	return 0;
 }
