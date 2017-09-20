c086d9bcc889df2aecb8683530fb88e9ff2b12e8
Thiagotrfm <t_moreira@outlook.com>
2017-09-19 21:28:16 -0300

    Dados básicos e leitura

--- multiplode3.cpp
+++ multiplode3.cpp
@@ -2,8 +2,16 @@
 
 using namespace std;
 
+long somaDosDigitos(String numero){
+	
+}
+
 int main(){
+	String numero;
+	long somaDosDigitos;
+
+	cin >> numero;
 	
-	
+	somaDosDigitos = somarDigitos(numero);
 	return 0;
 }
